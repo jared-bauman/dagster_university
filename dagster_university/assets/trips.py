68b2c8f03a9fda4dc2b1e117023e7146f55547fe
jared-bauman <jaredjbauman@gmail.com>
2023-10-15 09:19:02 -0700

    Exercises 1-5

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -1,6 +1,41 @@
-import requests
+import requests, duckdb, os
 from . import constants
 from dagster import asset
+
+@asset
+def taxi_zones_file():
+    """
+	The raw parquet files for the taxi zones dataset. From the NYC Open Data portal.
+    """
+    raw_zones = requests.get(
+        f"https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_zones.content)
+
+
+@asset(
+	deps=["taxi_zones_file"]
+)
+def taxi_zones():
+		"""
+        The raw taxi zones dataset, loaded into a DuckDB database
+    """
+		sql_query = f"""
+				create or replace table zones as (
+						select
+                                                    LocationID as zone_id,
+                                                    zone,
+                                                    borough,
+                                                    the_geom as geometry
+						from '{constants.TAXI_ZONES_FILE_PATH}'
+				);
+		"""
+
+		conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+		conn.execute(sql_query)
+
+
 
 @asset
 def taxi_trips_file():
@@ -14,3 +49,30 @@ def taxi_trips_file():
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
+
+@asset(
+	deps=["taxi_trips_file"]
+)
+def taxi_trips():
+		"""
+        The raw taxi trips dataset, loaded into a DuckDB database
+    """
+		sql_query = """
+				create or replace table trips as (
+						select
+								VendorID as vendor_id,
+								PULocationID as pickup_zone_id,
+								DOLocationID as dropoff_zone_id,
+								RatecodeID as rate_code_id,
+								payment_type as payment_type,
+								tpep_dropoff_datetime as dropoff_datetime,
+								tpep_pickup_datetime as pickup_datetime,
+								trip_distance as trip_distance,
+								passenger_count as passenger_count,
+								total_amount as total_amount
+						from 'data/raw/taxi_trips_2023-03.parquet'
+				);
+		"""
+
+		conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+		conn.execute(sql_query)
